22fe7fbaf378f71619965062871ce09c8758ef1a
Wesley Mauk <WesleyM77@users.noreply.github.com>
2017-07-25 15:12:19 -0400

    Fixes
    Forced the bot to save upon commenting since it constantly checks and never finishes.
    Bot lets you know every 100 posts that it's still chuggin' away.
    Moved comment to one place so future changes are consistent.

--- QueenMarchesaLMSRbot.py
+++ QueenMarchesaLMSRbot.py
@@ -5,8 +5,12 @@ import os
 import time
 
 reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit("magictcg+edh+pythonforengineers")
 
-subreddit = reddit.subreddit("magictcg+edh")
+lmsrcomment = """>Queen Marchesa (long may she reign)\n
+                    \nFTFY. I'm a bot. If I've made a mistake, click [here.]
+                    (https://www.reddit.com/message/compose?to=shadowwesley77)
+                    """
         
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
@@ -24,13 +28,21 @@ else:
        comments_replied_to = comments_replied_to.split("\n")
        comments_replied_to = list(filter(None, comments_replied_to))
 
+checked = 0
+
 #Check submission titles
 for submission in subreddit.stream.submissions():
     if submission.id not in posts_replied_to:
+        checked = checked + 1
+        if checked % 100 == 100:
+            print ("Checked ", checked ," posts")
         if re.search("Queen Marchesa", submission.title, re.IGNORECASE) and not re.search("long may she reign", submission.title, re.IGNORECASE):
-            submission.reply(">Queen Marchesa (long may she reign)\n\nFTFY. I'm a bot. If I've made a mistake, click [here.](https://www.reddit.com/message/compose?to=shadowwesley77)")
+            submission.reply(lmsrcomment)
             print("Bot replied to: ", submission.title)
             posts_replied_to.append(submission.id)
+            with open("posts_replied_to.txt", "w") as f:
+                for post_id in posts_replied_to:
+                    f.write(post_id + "\n")
         #Check comments of post
         submission.comments.replace_more(limit=0)
         comments = submission.comments[:]
@@ -38,18 +50,17 @@ for submission in subreddit.stream.submissions():
             comment = comments.pop(0)
             if comment.id not in comments_replied_to and comment.author is not "MTGCardFetcher":
                 if re.search("Queen Marchesa", comment.body, re.IGNORECASE) and not re.search("long may she reign", comment.body, re.IGNORECASE):
-                    comment.reply(">Queen Marchesa (long may she reign)\n\nFTFY. I'm a bot. If I've made a mistake, click [here.](https://www.reddit.com/message/compose?to=shadowwesley77)")
+                    comment.reply(lmsrcomment)
                     print("Bot replied to comment under: ", submission.title)
                     comments_replied_to.append(comment.id)
+                    with open("comments_replied_to.txt", "w") as t:
+                        for post_id in comments_replied_to:
+                            t.write(post_id + "\n")
                 comments.extend(comment.replies)
 
-with open("posts_replied_to.txt", "w") as f:
-    for post_id in posts_replied_to:
-        f.write(post_id + "\n")
 
-with open("comments_replied_to.txt", "w") as t:
-    for post_id in comments_replied_to:
-        t.write(post_id + "\n")
+
+
 
 
             
