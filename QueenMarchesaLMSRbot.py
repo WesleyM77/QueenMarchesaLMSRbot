8288be3226f1f90aa612f89d783f46ba6d5b0704
Wesley Mauk <WesleyM77@users.noreply.github.com>
2017-07-26 11:27:44 -0400

    Minor Changes
    Made the comment a little less snarky.
    Forced the bot to fetch new posts after it checks the 100 it grabbed. (It will recheck posts which is good as it monitors conversation)
    Added a status message that gets sent to me every hour.
    Added a switch so the bot goes back and forth between checking 100 new posts and 100 hot posts.

--- QueenMarchesaLMSRbot.py
+++ QueenMarchesaLMSRbot.py
@@ -4,11 +4,13 @@ import re
 import os
 import time
 
+start = time.time()
+
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("magictcg+edh+pythonforengineers")
 
 lmsrcomment = """>Queen Marchesa (long may she reign)\n
-                    \nFTFY. I'm a bot. If I've made a mistake, click [here.]
+                    \nPlease address the Queen with respect. I'm a bot. If I've made a mistake, click [here.]
                     (https://www.reddit.com/message/compose?to=shadowwesley77)
                     """
         
@@ -29,35 +31,68 @@ else:
        comments_replied_to = list(filter(None, comments_replied_to))
 
 checked = 0
+replied = 0
+switch = False
 
 #Check submission titles
-for submission in subreddit.stream.submissions():
-    if submission.id not in posts_replied_to:
-        checked = checked + 1
-        if checked % 100 == 0:
-            print ("Checked ", checked ," posts")
-        if re.search("Queen Marchesa", submission.title, re.IGNORECASE) and not re.search("long may she reign", submission.title, re.IGNORECASE):
-            submission.reply(lmsrcomment)
-            print("Bot replied to: ", submission.title)
-            posts_replied_to.append(submission.id)
-            with open("posts_replied_to.txt", "w") as f:
-                for post_id in posts_replied_to:
-                    f.write(post_id + "\n")
-        #Check comments of post
-        submission.comments.replace_more(limit=0)
-        comments = submission.comments[:]
-        while comments:
-            comment = comments.pop(0)
-            if comment.id not in comments_replied_to and comment.author is not "MTGCardFetcher":
-                if re.search("Queen Marchesa", comment.body, re.IGNORECASE) and not re.search("long may she reign", comment.body, re.IGNORECASE):
-                    comment.reply(lmsrcomment)
-                    print("Bot replied to comment under: ", submission.title)
-                    comments_replied_to.append(comment.id)
-                    with open("comments_replied_to.txt", "w") as t:
-                        for post_id in comments_replied_to:
-                            t.write(post_id + "\n")
-                comments.extend(comment.replies)
-
+should_restart = True
+while should_restart:
+    should_restart = False
+    if switch == True:
+        posts = subreddit.new(limit=100)
+        switch = False
+    else:
+        posts = subreddit.hot(limit=100)
+        switch = True
+    for submission in posts:
+        if submission.id not in posts_replied_to:
+            checked = checked + 1
+            print ("Checked ", checked ," posts. ", submission.title)
+            if time.time() - start % 3600 == 0:
+                msg = "Uptime: " + ((time.time()-start)/3600) + " hours.\n\n Checked " + checked + " posts.\n\nReplied to " + replied + " posts."
+                reddit.redditor('shadowwesley77').message("Bot Status", msg,) 
+            if checked % 100 == 0:
+                should_restart = True
+                break
+            if re.search("Queen Marchesa", submission.title, re.IGNORECASE) and not re.search("long may she reign", submission.title, re.IGNORECASE):
+                submission.reply(lmsrcomment)
+                replied += 1
+                print("Bot replied to: ", submission.title)
+                posts_replied_to.append(submission.id)
+                with open("posts_replied_to.txt", "w") as f:
+                    for post_id in posts_replied_to:
+                        f.write(post_id + "\n")
+            #Check comments of post
+            submission.comments.replace_more(limit=0)
+            comments = submission.comments[:]
+            while comments:
+                comment = comments.pop(0)
+                if comment.id not in comments_replied_to and comment.author is not "MTGCardFetcher":
+                    if re.search("Queen Marchesa", comment.body, re.IGNORECASE) and not re.search("long may she reign", comment.body, re.IGNORECASE):
+                        comment.reply(lmsrcomment)
+                        replied += 1
+                        print("Bot replied to comment under: ", submission.title)
+                        comments_replied_to.append(comment.id)
+                        with open("comments_replied_to.txt", "w") as t:
+                            for post_id in comments_replied_to:
+                                t.write(post_id + "\n")
+                    comments.extend(comment.replies)
+        else:
+            #Check comments of post
+            submission.comments.replace_more(limit=0)
+            comments = submission.comments[:]
+            while comments:
+                comment = comments.pop(0)
+                if comment.id not in comments_replied_to and comment.author is not "MTGCardFetcher":
+                    if re.search("Queen Marchesa", comment.body, re.IGNORECASE) and not re.search("long may she reign", comment.body, re.IGNORECASE):
+                        comment.reply(lmsrcomment)
+                        replied += 1
+                        print("Bot replied to comment under: ", submission.title)
+                        comments_replied_to.append(comment.id)
+                        with open("comments_replied_to.txt", "w") as t:
+                            for post_id in comments_replied_to:
+                                t.write(post_id + "\n")
+                    comments.extend(comment.replies)
 
 
 
